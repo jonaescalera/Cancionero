61b728604053b12e399be732cd449d3389588b37
Lucas Contreras <lcontreras@gyfsi.com.ar>
2018-10-31 16:02:12 -0300

    cambios varios

--- App.js
+++ App.js
@@ -1,36 +1,55 @@
 'use strict';
 import React, { Component } from 'react'
 import {
-    Text,
-    View,
-    StyleSheet,
-    Image
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  ImageBackground
 } from 'react-native'
 
 import ListSong from './src/components/ListSong'
 import Song from './src/components/Song'
-import {StackNavigator} from 'react-navigation'
-import {Fonts} from './src/utils/Fonts'
+import { StackNavigator } from 'react-navigation'
+import { Fonts } from './src/utils/Fonts'
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const AppNavigator = StackNavigator({
-  ListSong:{screen: ListSong},
-  Song:{screen: Song}
-},{
-  headerMode: 'screen'
-}
-)
+  ListSong: { 
+    screen: ListSong,
+    navigationOptions: ({navigation}) => {
+      
+    }
+   },
+  Song: { screen: Song }
+}, {
+    initialRouteName: "ListSong",
+    headerMode: 'screen',
+    navigationOptions: ({ navigation }) => (
+      {
+        headerLeft: <View><Icon name="arrow-left" size={30}></Icon></View>,
+        header: (
+          <ImageBackground source={require('./src/images/imagenTrasera.jpg')}
+            style={{ flex: 1, width: null, height: null }}>
+            
+          </ImageBackground>
+        )
+      }
+    )
+  }
+);
 
 class App extends Component {
   constructor(props) {
-      super(props)     
+    super(props)
   }
   render() {
-      console.disableYellowBox = true;
-      
-      return (
-              <AppNavigator />             
+    console.disableYellowBox = true;
 
-      )
+    return (
+      <AppNavigator />
+
+    )
   }
 }
 
--- src/components/ListSong.js
+++ src/components/ListSong.js
@@ -1,102 +1,130 @@
 import React from "react";
 import { FlatList, TextInput, StyleSheet, View } from "react-native";
 import { Text, ListItem, Left, Body, Right } from "native-base";
-import Icon from 'react-native-vector-icons/Entypo'
-import {Fonts} from '../utils/Fonts'
+import Icon from "react-native-vector-icons/Ionicons";
+import { Fonts } from "../utils/Fonts";
+import { Input } from "react-native-elements";
 //import { Icon as icono } from 'react-native-vector-icons/Entypo'
 
-
 //const data = require('../data/data.json')
-import customData from '../data/data.json'
+import customData from "../data/data.json";
 import { white, black } from "ansi-colors";
 
 export default class ListSong extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      data: customData.data,
-      dataClon: undefined,
-      stickyHeaderIndices: [],
-      text: '',
-      hayRegistros: true,
-      cambiarIcono: true
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			data: customData.data,
+			dataClon: undefined,
+			stickyHeaderIndices: [],
+			text: "",
+			hayRegistros: true,
+			cambiarIcono: true
+		};
+	}
 
+	renderItem = ({ item }) => {
+		return (
+			<ListItem
+				onPress={() =>
+					this.props.navigation.navigate("Song", {
+						item: item.lyrics,
+						titulo: item.name
+					})
+				}
+				style={{ marginLeft: 0 }}
+			>
+				<Body>
+					<Text style={styles.letra}>{item.name}</Text>
+				</Body>
+				<Right />
+			</ListItem>
+		);
+	};
 
-  renderItem = ({ item }) => {    
-      return (
-        <ListItem onPress={() => this.props.navigation.navigate('Song', { item: item.lyrics, titulo: item.name })} style={{ marginLeft: 0 }}>
-          <Body>
-            <Text style={styles.letra}>{item.name}</Text>
-          </Body>
-          <Right>            
-          </Right>
-        </ListItem>
-      );    
-  };
+	filterData(text) {
+		let textLowerCase = text.toLowerCase();
+		const a = this.state.data.filter(
+			(d) => d.name.toLowerCase().indexOf(textLowerCase) > -1
+		);
 
-  filterData(text) {
+		this.setState({
+			dataClon: a,
+			text: text,
+			hayRegistros: a.length > 0
+		});
+	}
 
-    let textLowerCase = text.toLowerCase();
-    const a = this.state.data.filter(d => d.name.toLowerCase().indexOf(textLowerCase) > -1);
+	channgeIcon() {
+		this.setState({
+			cambiarIcono: !cambiarIcono
+		});
+	}
 
+	render() {
+		const { navigate } = this.props.navigation;
+		return (
+			<View style={styles.searchSection}>
+				<TextInput
+					style={styles.textInput}
+					onChangeText={(text) => this.filterData(text)}
+					value={this.state.text}
+					placeholder="Busca una canción"
+					placeholderTextColor="white"
+					underlineColorAndroid="transparent"
+				/>
+				<Icon
+					style={styles.searchIcon}
+					name="ios-search"
+					size={20}
+					color="white"
+				/>
 
-    this.setState({
-      dataClon: a,
-      text: text,
-      hayRegistros: a.length > 0
-    });
-
-  }
-
-  channgeIcon(){
-    this.setState({
-      cambiarIcono: !cambiarIcono
-    })
-  }
-
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      
-      <View style={{flex:1,backgroundColor:"black"}}>     
-        <TextInput style={styles.textInput}
-          onChangeText={(text) => this.filterData(text)}
-          value={this.state.text}
-          placeholder="Buscar"
-          placeholderTextColor="white"
-        />
-        {(this.state.hayRegistros) ?
-          <FlatList 
-            data={this.state.dataClon || this.state.data}
-            renderItem={this.renderItem}
-            keyExtractor={item => item.name}
-            stickyHeaderIndices={this.state.stickyHeaderIndices}
-          /> :
-          <Text style={styles.textElementsNotFound}>Título no encontrado</Text>}
-      </View>
-    );
-  }
+				{this.state.hayRegistros ? (
+					<FlatList
+						data={this.state.dataClon || this.state.data}
+						renderItem={this.renderItem}
+						keyExtractor={(item) => item.name}
+						stickyHeaderIndices={this.state.stickyHeaderIndices}
+					/>
+				) : (
+					<Text style={styles.textElementsNotFound}>
+						Título no encontrado
+					</Text>
+				)}
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  textInput: {
-    height: 40,
-    borderWidth: 1,
-    borderColor: '#cecece',
-    marginBottom: 10,
-    marginHorizontal: 10,
-    fontSize: 18, 
-    color:'white'
-  },
-  textElementsNotFound: {
-    fontSize: 30,
-    marginLeft: 50,
-    color:'white'
-  },
-  letra:{
-    color: 'white',
-    fontFamily: Fonts.GothamMediumRegular
-  }
-})
+	textInput: {
+		height: 40,
+		borderWidth: 1,
+		borderColor: "#cecece",
+		marginBottom: 10,
+		marginHorizontal: 10,
+		fontSize: 18,
+		color: "white",
+		borderRadius: 25,
+		paddingLeft: 10
+	},
+	textElementsNotFound: {
+		fontSize: 30,
+		marginLeft: 50,
+		color: "white"
+	},
+	letra: {
+		color: "white",
+		fontFamily: Fonts.GothamMediumRegular
+	},
+	searchSection: {
+		flex: 1,
+
+		backgroundColor: "black"
+	},
+	searchIcon: {
+		paddingBottom: 10,
+		paddingLeft: 350
+	}
+});
